d6815c37b4682f8de402aca641cba9ef8efd27ae
JR-Bam <110076336+JR-Bam@users.noreply.github.com>
2026-02-26 15:32:21 +0800

    made thresholds part of world config and integration

--- Biome Generation/biome_map.gd
+++ Biome Generation/biome_map.gd
@@ -50,6 +50,10 @@ func _update_maps():
 	simplex_shader.set_shader_parameter("temperature_map", simplex_temperature)
 	simplex_shader.set_shader_parameter("precipitation_map", simplex_precipitation)
 	simplex_shader.set_shader_parameter("humidity_map", simplex_humidity)
+	
+	# Set all threshold parameters for simplex shader
+	_set_biome_thresholds(simplex_shader, Config)
+	
 	simplex_rect.material = simplex_shader
 	
 	var perlin_shader := ShaderMaterial.new()
@@ -58,7 +62,114 @@ func _update_maps():
 	perlin_shader.set_shader_parameter("temperature_map", perlin_temperature)
 	perlin_shader.set_shader_parameter("precipitation_map", perlin_precipitation)
 	perlin_shader.set_shader_parameter("humidity_map", perlin_humidity)
+	
+	# Set all threshold parameters for perlin shader
+	_set_biome_thresholds(perlin_shader, Config)
+	
 	perlin_rect.material = perlin_shader
+
+func _set_biome_thresholds(shader_material: ShaderMaterial, config: WorldConfigResource):
+	# Ocean Thresholds
+	shader_material.set_shader_parameter("ocean_e_min", config.ocean_e_min)
+	shader_material.set_shader_parameter("ocean_e_max", config.ocean_e_max)
+	shader_material.set_shader_parameter("ocean_t_min", config.ocean_t_min)
+	shader_material.set_shader_parameter("ocean_t_max", config.ocean_t_max)
+	shader_material.set_shader_parameter("ocean_p_min", config.ocean_p_min)
+	shader_material.set_shader_parameter("ocean_p_max", config.ocean_p_max)
+	shader_material.set_shader_parameter("ocean_h_min", config.ocean_h_min)
+	shader_material.set_shader_parameter("ocean_h_max", config.ocean_h_max)
+	
+	# Desert Thresholds
+	shader_material.set_shader_parameter("desert_e_min", config.desert_e_min)
+	shader_material.set_shader_parameter("desert_e_max", config.desert_e_max)
+	shader_material.set_shader_parameter("desert_t_min", config.desert_t_min)
+	shader_material.set_shader_parameter("desert_t_max", config.desert_t_max)
+	shader_material.set_shader_parameter("desert_p_min", config.desert_p_min)
+	shader_material.set_shader_parameter("desert_p_max", config.desert_p_max)
+	shader_material.set_shader_parameter("desert_h_min", config.desert_h_min)
+	shader_material.set_shader_parameter("desert_h_max", config.desert_h_max)
+	
+	# Grassland Thresholds
+	shader_material.set_shader_parameter("grassland_e_min", config.grassland_e_min)
+	shader_material.set_shader_parameter("grassland_e_max", config.grassland_e_max)
+	shader_material.set_shader_parameter("grassland_t_min", config.grassland_t_min)
+	shader_material.set_shader_parameter("grassland_t_max", config.grassland_t_max)
+	shader_material.set_shader_parameter("grassland_p_min", config.grassland_p_min)
+	shader_material.set_shader_parameter("grassland_p_max", config.grassland_p_max)
+	shader_material.set_shader_parameter("grassland_h_min", config.grassland_h_min)
+	shader_material.set_shader_parameter("grassland_h_max", config.grassland_h_max)
+	
+	# Savanna Thresholds
+	shader_material.set_shader_parameter("savanna_e_min", config.savanna_e_min)
+	shader_material.set_shader_parameter("savanna_e_max", config.savanna_e_max)
+	shader_material.set_shader_parameter("savanna_t_min", config.savanna_t_min)
+	shader_material.set_shader_parameter("savanna_t_max", config.savanna_t_max)
+	shader_material.set_shader_parameter("savanna_p_min", config.savanna_p_min)
+	shader_material.set_shader_parameter("savanna_p_max", config.savanna_p_max)
+	shader_material.set_shader_parameter("savanna_h_min", config.savanna_h_min)
+	shader_material.set_shader_parameter("savanna_h_max", config.savanna_h_max)
+	
+	# Tundra Thresholds
+	shader_material.set_shader_parameter("tundra_e_min", config.tundra_e_min)
+	shader_material.set_shader_parameter("tundra_e_max", config.tundra_e_max)
+	shader_material.set_shader_parameter("tundra_t_min", config.tundra_t_min)
+	shader_material.set_shader_parameter("tundra_t_max", config.tundra_t_max)
+	shader_material.set_shader_parameter("tundra_p_min", config.tundra_p_min)
+	shader_material.set_shader_parameter("tundra_p_max", config.tundra_p_max)
+	shader_material.set_shader_parameter("tundra_h_min", config.tundra_h_min)
+	shader_material.set_shader_parameter("tundra_h_max", config.tundra_h_max)
+	
+	# Boreal Forest Thresholds
+	shader_material.set_shader_parameter("boreal_forest_e_min", config.boreal_forest_e_min)
+	shader_material.set_shader_parameter("boreal_forest_e_max", config.boreal_forest_e_max)
+	shader_material.set_shader_parameter("boreal_forest_t_min", config.boreal_forest_t_min)
+	shader_material.set_shader_parameter("boreal_forest_t_max", config.boreal_forest_t_max)
+	shader_material.set_shader_parameter("boreal_forest_p_min", config.boreal_forest_p_min)
+	shader_material.set_shader_parameter("boreal_forest_p_max", config.boreal_forest_p_max)
+	shader_material.set_shader_parameter("boreal_forest_h_min", config.boreal_forest_h_min)
+	shader_material.set_shader_parameter("boreal_forest_h_max", config.boreal_forest_h_max)
+	
+	# Temperate Forest Thresholds
+	shader_material.set_shader_parameter("temperate_forest_e_min", config.temperate_forest_e_min)
+	shader_material.set_shader_parameter("temperate_forest_e_max", config.temperate_forest_e_max)
+	shader_material.set_shader_parameter("temperate_forest_t_min", config.temperate_forest_t_min)
+	shader_material.set_shader_parameter("temperate_forest_t_max", config.temperate_forest_t_max)
+	shader_material.set_shader_parameter("temperate_forest_p_min", config.temperate_forest_p_min)
+	shader_material.set_shader_parameter("temperate_forest_p_max", config.temperate_forest_p_max)
+	shader_material.set_shader_parameter("temperate_forest_h_min", config.temperate_forest_h_min)
+	shader_material.set_shader_parameter("temperate_forest_h_max", config.temperate_forest_h_max)
+	
+	# Rainforest Thresholds
+	shader_material.set_shader_parameter("rainforest_e_min", config.rainforest_e_min)
+	shader_material.set_shader_parameter("rainforest_e_max", config.rainforest_e_max)
+	shader_material.set_shader_parameter("rainforest_t_min", config.rainforest_t_min)
+	shader_material.set_shader_parameter("rainforest_t_max", config.rainforest_t_max)
+	shader_material.set_shader_parameter("rainforest_p_min", config.rainforest_p_min)
+	shader_material.set_shader_parameter("rainforest_p_max", config.rainforest_p_max)
+	shader_material.set_shader_parameter("rainforest_h_min", config.rainforest_h_min)
+	shader_material.set_shader_parameter("rainforest_h_max", config.rainforest_h_max)
+	
+	# Mountain Thresholds
+	shader_material.set_shader_parameter("mountain_e_min", config.mountain_e_min)
+	shader_material.set_shader_parameter("mountain_e_max", config.mountain_e_max)
+	shader_material.set_shader_parameter("mountain_t_min", config.mountain_t_min)
+	shader_material.set_shader_parameter("mountain_t_max", config.mountain_t_max)
+	shader_material.set_shader_parameter("mountain_p_min", config.mountain_p_min)
+	shader_material.set_shader_parameter("mountain_p_max", config.mountain_p_max)
+	shader_material.set_shader_parameter("mountain_h_min", config.mountain_h_min)
+	shader_material.set_shader_parameter("mountain_h_max", config.mountain_h_max)
+	
+	# Alpine Tundra Thresholds
+	shader_material.set_shader_parameter("alpine_tundra_e_min", config.alpine_tundra_e_min)
+	shader_material.set_shader_parameter("alpine_tundra_e_max", config.alpine_tundra_e_max)
+	shader_material.set_shader_parameter("alpine_tundra_t_min", config.alpine_tundra_t_min)
+	shader_material.set_shader_parameter("alpine_tundra_t_max", config.alpine_tundra_t_max)
+	shader_material.set_shader_parameter("alpine_tundra_p_min", config.alpine_tundra_p_min)
+	shader_material.set_shader_parameter("alpine_tundra_p_max", config.alpine_tundra_p_max)
+	shader_material.set_shader_parameter("alpine_tundra_h_min", config.alpine_tundra_h_min)
+	shader_material.set_shader_parameter("alpine_tundra_h_max", config.alpine_tundra_h_max)
+	
+	shader_material.set_shader_parameter("blend_radius", config.blend_radius)
 
 func _noise_to_texture(size, texture, noise):
 	texture.set_width(size)
--- main.gd
+++ main.gd
@@ -19,7 +19,7 @@ class_name WorldConfigResource
 
 @export_group("Desert Thresholds")
 @export_range(0.0, 1.0, 0.01) var desert_e_min: float = 0.15
-@export_range(0.0, 1.0, 0.01) var desert_e_max: float = 0.8
+@export_range(0.0, 1.0, 0.01) var desert_e_max: float = 0.9
 @export_range(0.0, 1.0, 0.01) var desert_t_min: float = 0.6
 @export_range(0.0, 1.0, 0.01) var desert_t_max: float = 1.0
 @export_range(0.0, 1.0, 0.01) var desert_p_min: float = 0.0
@@ -29,47 +29,47 @@ class_name WorldConfigResource
 
 @export_group("Grassland Thresholds")
 @export_range(0.0, 1.0, 0.01) var grassland_e_min: float = 0.2
-@export_range(0.0, 1.0, 0.01) var grassland_e_max: float = 0.7
+@export_range(0.0, 1.0, 0.01) var grassland_e_max: float = 0.9
 @export_range(0.0, 1.0, 0.01) var grassland_t_min: float = 0.25
 @export_range(0.0, 1.0, 0.01) var grassland_t_max: float = 0.65
-@export_range(0.0, 1.0, 0.01) var grassland_p_min: float = 0.25
-@export_range(0.0, 1.0, 0.01) var grassland_p_max: float = 0.65
-@export_range(0.0, 1.0, 0.01) var grassland_h_min: float = 0.25
-@export_range(0.0, 1.0, 0.01) var grassland_h_max: float = 0.65
+@export_range(0.0, 1.0, 0.01) var grassland_p_min: float = 0.0
+@export_range(0.0, 1.0, 0.01) var grassland_p_max: float = 1.0
+@export_range(0.0, 1.0, 0.01) var grassland_h_min: float = 0.0
+@export_range(0.0, 1.0, 0.01) var grassland_h_max: float = 1.0
 
 @export_group("Savanna Thresholds")
 @export_range(0.0, 1.0, 0.01) var savanna_e_min: float = 0.15
-@export_range(0.0, 1.0, 0.01) var savanna_e_max: float = 0.65
+@export_range(0.0, 1.0, 0.01) var savanna_e_max: float = 0.9
 @export_range(0.0, 1.0, 0.01) var savanna_t_min: float = 0.6
 @export_range(0.0, 1.0, 0.01) var savanna_t_max: float = 1.0
 @export_range(0.0, 1.0, 0.01) var savanna_p_min: float = 0.3
-@export_range(0.0, 1.0, 0.01) var savanna_p_max: float = 0.65
+@export_range(0.0, 1.0, 0.01) var savanna_p_max: float = 1.0
 @export_range(0.0, 1.0, 0.01) var savanna_h_min: float = 0.3
-@export_range(0.0, 1.0, 0.01) var savanna_h_max: float = 0.65
+@export_range(0.0, 1.0, 0.01) var savanna_h_max: float = 1.0
 
 @export_group("Tundra Thresholds")
-@export_range(0.0, 1.0, 0.01) var tundra_e_min: float = 0.4
+@export_range(0.0, 1.0, 0.01) var tundra_e_min: float = 0.2
 @export_range(0.0, 1.0, 0.01) var tundra_e_max: float = 0.9
 @export_range(0.0, 1.0, 0.01) var tundra_t_min: float = 0.0
 @export_range(0.0, 1.0, 0.01) var tundra_t_max: float = 0.35
 @export_range(0.0, 1.0, 0.01) var tundra_p_min: float = 0.0
-@export_range(0.0, 1.0, 0.01) var tundra_p_max: float = 0.7
+@export_range(0.0, 1.0, 0.01) var tundra_p_max: float = 1.0
 @export_range(0.0, 1.0, 0.01) var tundra_h_min: float = 0.0
-@export_range(0.0, 1.0, 0.01) var tundra_h_max: float = 0.45
+@export_range(0.0, 1.0, 0.01) var tundra_h_max: float = 1.0
 
 @export_group("Boreal Forest Thresholds")
 @export_range(0.0, 1.0, 0.01) var boreal_forest_e_min: float = 0.2
-@export_range(0.0, 1.0, 0.01) var boreal_forest_e_max: float = 0.6
-@export_range(0.0, 1.0, 0.01) var boreal_forest_t_min: float = 0.0
+@export_range(0.0, 1.0, 0.01) var boreal_forest_e_max: float = 0.9
+@export_range(0.0, 1.0, 0.01) var boreal_forest_t_min: float = 0.35
 @export_range(0.0, 1.0, 0.01) var boreal_forest_t_max: float = 0.5
-@export_range(0.0, 1.0, 0.01) var boreal_forest_p_min: float = 0.4
-@export_range(0.0, 1.0, 0.01) var boreal_forest_p_max: float = 0.75
-@export_range(0.0, 1.0, 0.01) var boreal_forest_h_min: float = 0.4
-@export_range(0.0, 1.0, 0.01) var boreal_forest_h_max: float = 0.75
+@export_range(0.0, 1.0, 0.01) var boreal_forest_p_min: float = 0.0
+@export_range(0.0, 1.0, 0.01) var boreal_forest_p_max: float = 1.0
+@export_range(0.0, 1.0, 0.01) var boreal_forest_h_min: float = 0.0
+@export_range(0.0, 1.0, 0.01) var boreal_forest_h_max: float = 1.0
 
 @export_group("Temperate Forest Thresholds")
 @export_range(0.0, 1.0, 0.01) var temperate_forest_e_min: float = 0.2
-@export_range(0.0, 1.0, 0.01) var temperate_forest_e_max: float = 0.65
+@export_range(0.0, 1.0, 0.01) var temperate_forest_e_max: float = 0.9
 @export_range(0.0, 1.0, 0.01) var temperate_forest_t_min: float = 0.3
 @export_range(0.0, 1.0, 0.01) var temperate_forest_t_max: float = 0.7
 @export_range(0.0, 1.0, 0.01) var temperate_forest_p_min: float = 0.6
@@ -79,7 +79,7 @@ class_name WorldConfigResource
 
 @export_group("Rainforest Thresholds")
 @export_range(0.0, 1.0, 0.01) var rainforest_e_min: float = 0.15
-@export_range(0.0, 1.0, 0.01) var rainforest_e_max: float = 0.5
+@export_range(0.0, 1.0, 0.01) var rainforest_e_max: float = 0.9
 @export_range(0.0, 1.0, 0.01) var rainforest_t_min: float = 0.65
 @export_range(0.0, 1.0, 0.01) var rainforest_t_max: float = 1.0
 @export_range(0.0, 1.0, 0.01) var rainforest_p_min: float = 0.7
@@ -91,7 +91,7 @@ class_name WorldConfigResource
 @export_range(0.0, 1.0, 0.01) var mountain_e_min: float = 0.9
 @export_range(0.0, 1.0, 0.01) var mountain_e_max: float = 1.0
 @export_range(0.0, 1.0, 0.01) var mountain_t_min: float = 0.0
-@export_range(0.0, 1.0, 0.01) var mountain_t_max: float = 0.4
+@export_range(0.0, 1.0, 0.01) var mountain_t_max: float = 1.0
 @export_range(0.0, 1.0, 0.01) var mountain_p_min: float = 0.0
 @export_range(0.0, 1.0, 0.01) var mountain_p_max: float = 1.0
 @export_range(0.0, 1.0, 0.01) var mountain_h_min: float = 0.0
@@ -101,10 +101,10 @@ class_name WorldConfigResource
 @export_range(0.0, 1.0, 0.01) var alpine_tundra_e_min: float = 0.75
 @export_range(0.0, 1.0, 0.01) var alpine_tundra_e_max: float = 0.95
 @export_range(0.0, 1.0, 0.01) var alpine_tundra_t_min: float = 0.0
-@export_range(0.0, 1.0, 0.01) var alpine_tundra_t_max: float = 0.25
+@export_range(0.0, 1.0, 0.01) var alpine_tundra_t_max: float = 0.35
 @export_range(0.0, 1.0, 0.01) var alpine_tundra_p_min: float = 0.0
-@export_range(0.0, 1.0, 0.01) var alpine_tundra_p_max: float = 0.6
+@export_range(0.0, 1.0, 0.01) var alpine_tundra_p_max: float = 1.0
 @export_range(0.0, 1.0, 0.01) var alpine_tundra_h_min: float = 0.0
-@export_range(0.0, 1.0, 0.01) var alpine_tundra_h_max: float = 0.5
+@export_range(0.0, 1.0, 0.01) var alpine_tundra_h_max: float = 1.0
 
 @export var blend_radius: float = 0.05
